49829b06b47769b338a753c273e1bcf5948f7add
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-07-05 14:50:53 +0200

    fix templates home and userProfile

--- src/views/templates/home.php
+++ src/views/templates/home.php
@@ -5,18 +5,51 @@
 
     $registerValidate = Utils::request('registerValidate');
 ?>
+
 <div class="alert">
     <?php if($registerValidate === "true") :?>
         <p>Votre compte a bien été enregistré !</p>
     <?php endif ?>
 </div>
+<hr>
+<div>
+    <h2>Rejoignez nos lecteurs passionnés</h2>
+    <p>Donnez une nouvelle vie à vos livre en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+    <button><a href="index.php?action=showAllBooks">Découvrir</a></button>
+    <img src="#" alt="photo pile de livres">
+</div>
+<hr>
 <div class="bookList">
+    <h2>Les derniers livres ajoutés</h2>
     <?php foreach($books as $book) { ?>
         <article>
             <img src="<?=$book->getUrlImg(); ?>" alt="Image <?=$book->getTitle(); ?>">
             <h2></h2>
         </article>
-
     <?php } ?>
+    <button><a href="">Voir tous les livres</a></button>
+</div>
+<hr>
+<div>
+    <h2>Comment ça marche ?</h2>
+    <p>&eacute;changer des livres avec TomTroc c&rsquo;est simple et amusant ! Suivez ces &eacute;tapes pour commencer :</p>
+    <div>Inscrivez-vous gratuitement sur notre plateforme.</div>
+    <div>Ajoutez les livres que vous souhaitez &eacute;changer à votre profil.</div>
+    <div>Parcourez les livres disponibles chez d&rsquo;autres membres.</div>
+    <div>Proposez un &eacute;change et discutez avec d&rsquo;autres passionn&eacute;s de lecture.</div>
+    <button><a href="">Voir tous les livres</a></button>  
+</div>
+<hr>
+<div>
+    <img src="#" alt="photo bibliothèque">
+    <h2>Nos valeurs</h2>
+    <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte de la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre déisr de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+    <br>
+    <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
+    <br>
+    <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+    <br>
+    <i>L'équipe Tom Troc</i>
+    <img src="#" alt="Logo Coeur">
 </div>
 
--- src/views/templates/userProfile.php
+++ src/views/templates/userProfile.php
@@ -1,17 +1,53 @@
 <div class="userInfo">
     <h2>Mon Compte</h2>
-    <h3><?= $userInfo->getUsername();?></h3>
+    <h3><?= $userInfo->getUsername(); ?></h3>
 
 </div>
 
+<div class="connection-form">
+    <form action="index.php?action=updateUser" method="post">
+        <h3>Vos informations personnelles</h3>
+        <div class="formGrid">
+            <label for="username">Pseudo</label>
+            <input type="text" name="username" id="username" value="<?= $userInfo->getUsername(); ?>">
+            <label for="mail">Adresse email</label>
+            <input type="text" name="mail" id="mail" value="<?= $userInfo->getMail(); ?>">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" value="<?= $userInfo->getPassword(); ?>">
+            <button class="submit">Enregistrer</button>
+        </div>
+    </form>
+</div>
+<hr>
+<a href="index.php?action=addBookForm">Ajouter un livre</a>
+<hr>
 <div class="bookList">
-    <?php foreach($books as $book) { ?>
-        <article>
-            <img src="<?=$book->getUrlImg(); ?>" alt="Image <?=$book->getTitle(); ?>">
-            <h2></h2>
-        </article>
+    <table>
+        <thead>
+            <th>Photo</th>
+            <th>Titre</th>
+            <th>Auteur</th>
+            <th>Resumé</th>
+            <th>Disponibilité</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            <?php foreach ($books as $book) { ?>
+            <tr>
+                <td><img src="<?= $book->getUrlImg(); ?>" alt="Image <?= $book->getTitle(); ?>"></td>
+                <td><?= $book->getTitle(); ?></td>
+                <td><?= $book->getAuthor(); ?></td>
+                <td><?= $book->getResume(); ?></td>
+                <td><?php if ($book->getIsEnable()) {
+                        echo 'disponible';
+                    } else {
+                        echo 'non-disponible';
+                    } ?></td>
+                <td><a href="#">Editer</a> <a href="">Suppimer</a></td>
+            </tr>
+            <?php } ?>
 
-    <?php } ?>
+        </tbody>
+
+    </table>
 </div>
-
-<a href="index.php?action=AddBookForm">Ajouter un livre</a>
